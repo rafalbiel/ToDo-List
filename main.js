c1b6aecbc6e2c219fa1933de6b2a8e1958f25d0d
unknown <rafalbiel77@gmail.com>
2022-10-25 18:34:01 +0200

    added delete button

--- main.js
+++ main.js
@@ -9,29 +9,43 @@ function renderTable() {
 	const trElement = document.createElement("tr");
 	const thElement1 = document.createElement("th");
 	const thElement2 = document.createElement("th");
+	const thElement3 = document.createElement("th");
 
 	thElement1.textContent = "zadanie";
 	thElement2.textContent = "status";
+	thElement3.textContent = "usuń zadanie";
 	trElement.appendChild(thElement1);
 	trElement.appendChild(thElement2);
+	trElement.appendChild(thElement3);
 
 	tableElement.appendChild(trElement);
 	tasks.forEach((task, index) => {
 		const trTaskElement = document.createElement("tr");
 		const tdTaskTextElement = document.createElement("td");
 		const tdTaskStatusElement = document.createElement("td");
+		const tdTaskDeleteElement = document.createElement("td");
 		const inputTaskCheckboxElement = document.createElement("input");
 		inputTaskCheckboxElement.setAttribute("type", "checkbox");
+		const deleteButtonElement = document.createElement("button");
+		deleteButtonElement.textContent = "usuń zadanie";
 
 		tdTaskTextElement.textContent = task.text;
 		inputTaskCheckboxElement.checked = task.status;
 		inputTaskCheckboxElement.onclick = () => {
 			tasks[index].status = !task.status;
 		};
+
+		deleteButtonElement.onclick = () => {
+			tasks.splice(index,1);
+			renderTable();
+		};
+
 		tdTaskStatusElement.appendChild(inputTaskCheckboxElement);
+		tdTaskDeleteElement.appendChild(deleteButtonElement);
 
 		trTaskElement.appendChild(tdTaskTextElement);
 		trTaskElement.appendChild(tdTaskStatusElement);
+		trTaskElement.appendChild(tdTaskDeleteElement);
 		tableElement.appendChild(trTaskElement);
 	});
 	rootElement.appendChild(tableElement);
